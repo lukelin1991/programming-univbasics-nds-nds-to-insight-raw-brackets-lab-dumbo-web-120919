ed0eb04f748821cdf813c2db310b74209265c468
Luke Lin <lukelin1991@yahoo.com>
2019-11-28 00:12:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,12 +12,9 @@ def directors_totals(nds)
     column_index = 0
     total_gross = 0
     while column_index < directors_database[row_index][:movies].length do
-
-
       total_gross += directors_database[row_index][:movies][column_index][:worldwide_gross]
       column_index += 1
     end
-
     result[directors_database[row_index][:name]] = total_gross
     row_index += 1
   end
